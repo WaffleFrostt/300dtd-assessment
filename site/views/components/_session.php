769d30122466e1b45a3126d737a7afee6c7dcb79
WaffleFrostt <117048016+WaffleFrostt@users.noreply.github.com>
2024-08-22 22:52:58 +1200

    Fixing up double ups

--- site/views/components/_session.php
+++
@@ -1,3 +0,0 @@
-<?php
-session_name('tmeldrumSessionSite');
-session_start();
--- site/views/components/info.php
+++
@@ -1,8 +0,0 @@
-<table>
-        <tr>
-            <th>To answer your question: </th>
-            <td><?= rand(0,1) ? 'NO' : 'YES' ?></td>
-        </tr>
-    </table>
-
-    
